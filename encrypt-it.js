1631523843b32ec71f4f37d58d7ab928d5350d4b
mxue26 <63764917+mxue26@users.noreply.github.com>
2023-05-19 21:21:45 -0700

    Update Homework 6

--- encrypt-it.js
+++ encrypt-it.js
@@ -20,20 +20,29 @@
 	console.log("Window loaded!");
 	
 	const button = document.getElementById("encrypt-it");
+	const reset  = document.getElementById("reset");
 	
-	button.addEventListener("click", shift_cypher);
+	button.addEventListener("click", encrypt);
+	reset.addEventListener("click", reset_text_areas);
   }
+
+  
 
   // Add any other functions in this area (you should not implement your
   // entire program in the init function, for similar reasons that
   // you shouldn't write an entire Java program in the main method).
-  function shift_cypher()
+  function reset_text_areas()
   {
-	  console.log("Button clicked!");
+	  let input 			= document.getElementById("input-text");
+	  let output			= document.getElementById("result");
 	  
-	  let input 	= document.getElementById("input-text").value.toLowerCase();
-	  let output	= document.getElementById("result");
-	  let result	= "";
+	  input.value 			= "";
+	  output.textContent	= "";
+  }
+  
+  function shift_cypher(input)
+  {
+	  let result = "";
 	  
 	  for (let index = 0; index < input.length; index++)
 	  {
@@ -49,7 +58,43 @@
 		  }
 	  }
 	  
-	  output.textContent = result;
+	  return result;
+  }
+  
+  function random_cypher(input)
+  {
+	  const chars	= "abcdefghijklmnopqrstuvwxyz";
+	  let   result 	= "";
+	  
+	  for (let index = 0; index < input.length; index++)
+	  {
+		  if ((input[index] < 'a') || (input[index] > 'z'))
+			  result += input[index];
+		  else
+		  {  
+			  result += chars.charAt(Math.floor(Math.random() * chars.length));
+		  }
+	  }
+	  
+	  return result;
+  }
+  
+  function encrypt()
+  {
+	  console.log("Button clicked!");
+	  
+	  let input 	= document.getElementById("input-text").value.toLowerCase();
+	  let output	= document.getElementById("result");
+	  let uppercase	= document.getElementById('all-caps').checked;
+	  let algorithm	= document.getElementById('cipher-type').value;
+	  let size		= document.querySelector('input[name="text-size"]:checked').value
+	  let result	= (algorithm === "shift") ? shift_cypher(input) : random_cypher(input);
+	  
+	  if (uppercase)
+		  result = result.toUpperCase();
+	  
+	  output.style.fontSize	= size;
+	  output.textContent 	= result;
   }
 
 })();
